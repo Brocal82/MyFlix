7ab6a00c88560477c4b6c6f6caae4b4c3c89826d
Brocal82 <antonioruizbrocal@gmail.com>
2023-07-02 12:26:37 +0200

    hashPassword
    If applied this commit will modify the endpoint to hash the password using the hashPasword function.

--- index.js
+++ index.js
@@ -269,6 +269,7 @@ let movies = [
 
 // Allow new users to Register (CREATE)
   app.post('/users', (req, res) => {
+    let hashedPassword = Users.hashedPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
@@ -277,7 +278,7 @@ let movies = [
           Users
             .create({
               Username: req.body.Username,
-              Password: req.body.Password,
+              Password: hashedPassword,
               Email: req.body.Email,
               Birthday: req.body.Birthday
             })
