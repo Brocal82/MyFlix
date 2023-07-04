28368c50fd43ae632575cc888d999746989a45c8
Brocal82 <antonioruizbrocal@gmail.com>
2023-07-04 11:18:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 // mongoose.connect('mongodb://127.0.0.1/MyFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
