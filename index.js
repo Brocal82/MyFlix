5ce156b5699a45fa40211b891d98705c9f4df5bf
Brocal82 <antonioruizbrocal@gmail.com>
2023-07-09 14:34:34 +0200

    force deploy to heroku

--- index.js
+++ index.js
@@ -27,10 +27,6 @@ const Users = Models.User;
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
-
-
-
 let users = [
   { 
     Username: "Antonio",
